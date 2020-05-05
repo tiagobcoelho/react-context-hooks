408beef46db762d4764338ac0038968ad5d31351
Tiago Baptista Coelho <tiagojnbaptistacoelho@gmail.com>
2020-05-05 13:53:42 +0100

    up to lesson 8

--- contextapp/src/App.js
+++ contextapp/src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import BookList from './components/BookList';
 import Navbar from './components/Navbar';
+import ThemeContextProvider from './contexts/ThemeContext';
+import ThemeToggle from './components/ThemeToggle';
+import AuthContextProvider from './contexts/AuthContext';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <BookList />
+      <ThemeContextProvider>
+        <AuthContextProvider>
+          <Navbar />
+          <BookList />
+          <ThemeToggle />
+        </AuthContextProvider>
+      </ThemeContextProvider>
+      
     </div>
   );
 }
---
+++ contextapp/src/components/ThemeToggle.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
+
+class ThemeToggle extends Component {
+
+    static contextType = ThemeContext;
+    render() {
+        const { toggleTheme } = this.context
+        
+      return(
+          <button onClick={toggleTheme}>Theme Toggle</button>
+      )
+    }
+}
+
+export default ThemeToggle
